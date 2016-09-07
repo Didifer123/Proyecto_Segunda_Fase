89d55ec7f2a39b7ff3053817d7cb8c10a60a1383
Karlie27 <karliemmg@gmail.com>
2016-09-07 07:54:12 -0600

    Add files via upload

--- Primera parte.c
+++ Primera parte.c
@@ -186,3 +186,587 @@ retornarCamino();
 }
 }
 }
+void interDerIzq(){
+    turn = rand()%2;                          // Random val, odd = 1, even = 0
+  if(turn==1){
+    giraDerechaAvanza();
+  } 
+  else if(turn==0){
+    giraIzquierdaAvanza();
+  } 
+}  
+void interDerAdel(){
+    turn = rand()%2;                          // Random val, odd = 1, even = 0
+  if(turn==1){
+    giraDerechaAvanza();
+  } 
+  else if(turn==0){
+    avanzaAdelante();
+  } 
+}  
+void interIzqAdel(){
+    turn = rand()%2;                          // Random val, odd = 1, even = 0
+  if(turn==1){
+    giraIzquierdaAvanza();
+  } 
+  else if(turn==0){
+    avanzaAdelante();
+  }  
+} 
+void interIzqDerAdel(){
+    turn = rand()%3;                          // Random val, odd = 1, even = 0
+  if(turn==0){
+    giraIzquierdaAvanza();
+  } 
+  else if(turn==1){
+    avanzaAdelante();
+  }
+  else if(turn==2){
+    giraDerechaAvanza();
+  }    
+}
+void retornarCamino(){
+  int tramoRegreso=0;
+  giraAtras();
+  
+/*  for(int x=contadorRecorrido-1;x>=0;x--){
+    if(recorridoInverso[x]!=5){
+    if(recorridoInverso[x]==4){
+      regreso=1;
+      avanzaAdelante(); 
+    } 
+    else if(recorridoInverso[x]==3){
+      regreso=1;
+      avanzaAdelante();
+      giraDerecha();
+    } 
+    else if(recorridoInverso[x]==2){
+      regreso=1;
+      avanzaAdelante();
+      giraIzquierda();
+    } 
+   contadorRecorrido=contadorRecorrido-1;
+   recorridoInverso[x]=0; 
+   tramoRegreso=tramoRegreso+1;
+   } 
+   else if(recorridoInverso[x]==5){
+    recorridoInverso[x]=0;
+    tramoRegreso=tramoRegreso+1; 
+    break;
+  }       
+  } 
+  regreso=0;*/
+  
+}  
+void cruce(){
+  for(int i=0;i<=3;i++){
+    if(cruces[numCruces-1][i]==0){
+     contadorCrucesDisponibles= contadorCrucesDisponibles+1;
+     if(i==0){
+       cruceAnterior[0]=1;
+     }   
+     else if(i==1){
+       cruceAnterior[1]=1;
+     } 
+     else if(i==2){
+       cruceAnterior[2]=1;
+     } 
+     else if(i==3){
+       cruceAnterior[3]=1;
+     }     
+    }
+    }
+    
+    
+     if(contadorCrucesDisponibles==2){
+       int cruce1=0;
+       int cruce2=0;
+       int segundoCruce=0;
+       
+      for(int z=0;z<=3;z++){
+    if(cruceAnterior[z]==1){
+     if(z==0){
+       if(segundoCruce==0){
+       cruce1=1;
+       segundoCruce=1;
+     } 
+     else{
+       cruce2=1;
+     }             
+     }
+     if(z==1){
+       if(segundoCruce==0){
+       cruce1=2;
+       segundoCruce=1;
+     } 
+     else{
+       cruce2=2;
+     }             
+     }
+     if(z==2){
+       if(segundoCruce==0){
+       cruce1=3;
+       segundoCruce=1;
+     } 
+     else{
+       cruce2=3;
+     }             
+     }
+     if(z==3){
+       if(segundoCruce==0){
+       cruce1=4;
+       segundoCruce=1;
+     } 
+     else{
+       cruce2=4;
+     }             
+     }
+   }
+ }
+ segundoCruce=0;
+ 
+ if((estadoCruce[numCruces-1]==1)&&(estado==3)){
+interDerAdel();     
+if (turn==1){
+  cruces[numCruces-1][1]=1;
+}  
+else{
+  cruces[numCruces-1][0]=1;
+}      
+       }   
+       else if((estadoCruce[numCruces-1]==1)&&(estado==2)){
+interIzqAdel(); 
+if (turn==1){
+  cruces[numCruces-1][2]=1;
+}  
+else{
+  cruces[numCruces-1][0]=1;
+} 
+       } 
+       else if((estadoCruce[numCruces-1]==1)&&(estado==4)){
+interDerIzq(); 
+if (turn==1){
+  cruces[numCruces-1][1]=1;
+}  
+else{
+  cruces[numCruces-1][2]=1;
+} 
+       }      
+       
+       
+ if((estadoCruce[numCruces-1]==2)&&(estado==3)){
+interDerIzq(); 
+if (turn==1){
+  cruces[numCruces-1][0]=1;
+}  
+else{
+  cruces[numCruces-1][3]=1;
+}             
+       }   
+       else if((estadoCruce[numCruces-1]==2)&&(estado==1)){
+interDerAdel();  
+if (turn==1){
+  cruces[numCruces-1][1]=1;
+}  
+else{
+  cruces[numCruces-1][0]=1;
+}  
+       } 
+       else if((estadoCruce[numCruces-1]==2)&&(estado==4)){
+interIzqAdel();   
+if (turn==1){
+  cruces[numCruces-1][2]=1;
+}  
+else{
+  cruces[numCruces-1][3]=1;
+}  
+       } 
+       
+       else if((estadoCruce[numCruces-1]==3)&&(estado==2)){
+         interDerIzq();  
+         if (turn==1){
+  cruces[numCruces-1][3]=1;
+}  
+else{
+  cruces[numCruces-1][0]=1;
+} 
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==1)){
+         interIzqAdel(); 
+         if (turn==1){
+  cruces[numCruces-1][2]=1;
+}  
+else{
+  cruces[numCruces-1][0]=1;
+} 
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==4)){
+        interDerAdel(); 
+        if (turn==1){
+  cruces[numCruces-1][2]=1;
+}  
+else{
+  cruces[numCruces-1][3]=1;
+} 
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==2)){
+         interDerAdel(); 
+         if (turn==1){
+  cruces[numCruces-1][3]=1;
+}  
+else{
+  cruces[numCruces-1][1]=1;
+} 
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==1)){
+         interDerIzq(); 
+         if (turn==1){
+  cruces[numCruces-1][1]=1;
+}  
+else{
+  cruces[numCruces-1][2]=1;
+} 
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==3)){
+         interIzqAdel();
+         if (turn==1){
+  cruces[numCruces-1][3]=1;
+}  
+else{
+  cruces[numCruces-1][2]=1;
+}  
+       }
+ }               
+    if(contadorCrucesDisponibles==1){
+      for(int z=0;z<=3;z++){
+    if(cruceAnterior[z]==1){
+     if(z==0){
+       cruces[numCruces-1][0]=1;
+       if((estadoCruce[numCruces-1]==1)&&(estado==3)){
+         giraDerechaAvanza();
+       }   
+       else if((estadoCruce[numCruces-1]==1)&&(estado==2)){
+         giraIzquierdaAvanza();
+       }    
+       else if((estadoCruce[numCruces-1]==2)&&(estado==3)){
+         giraDerechaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==2)&&(estado==1)){
+         avanzaAdelante();
+       } 
+       else if((estadoCruce[numCruces-1]==3)&&(estado==2)){
+         giraIzquierdaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==1)){
+         avanzaAdelante();
+       }
+               
+     }   
+     else if(z==1){
+       cruces[numCruces-1][1]=1;
+       if((estadoCruce[numCruces-1]==1)&&(estado==4)){
+         giraIzquierdaAvanza();
+       }   
+       else if((estadoCruce[numCruces-1]==1)&&(estado==2)){
+         avanzaAdelante();
+       }    
+       else if((estadoCruce[numCruces-1]==4)&&(estado==2)){
+         avanzaAdelante();
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==1)){
+         giraDerechaAvanza();
+       } 
+       else if((estadoCruce[numCruces-1]==3)&&(estado==4)){
+         giraIzquierdaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==1)){
+         giraDerechaAvanza();
+       }
+     } 
+     else if(z==2){
+       cruces[numCruces-1][2]=1;
+       if((estadoCruce[numCruces-1]==1)&&(estado==4)){
+         giraDerechaAvanza();
+       }   
+       else if((estadoCruce[numCruces-1]==1)&&(estado==3)){
+         avanzaAdelante();
+       }    
+       else if((estadoCruce[numCruces-1]==4)&&(estado==3)){
+         avanzaAdelante();
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==1)){
+         giraIzquierdaAvanza();
+       } 
+       else if((estadoCruce[numCruces-1]==3)&&(estado==4)){
+         giraDerechaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==1)){
+         giraIzquierdaAvanza();
+       } 
+     }     
+    }
+    
+  }     
+              
+  }
+  else if(contadorCrucesDisponibles==0){
+    retorno=1;
+    if((estadoCruce[numCruces-1]==1)&&(estado==4)){
+         avanzaAdelante();
+       }   
+       else if((estadoCruce[numCruces-1]==1)&&(estado==2)){
+         giraDerechaAvanza();
+       }    
+       else if((estadoCruce[numCruces-1]==1)&&(estado==3)){
+         giraIzquierdaAvanza();
+       }  
+       else if((estadoCruce[numCruces-1]==4)&&(estado==2)){
+         giraIzquierdaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==4)&&(estado==1)){
+         avanzaAdelante();
+       } 
+       else if((estadoCruce[numCruces-1]==4)&&(estado==3)){
+         giraDerechaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==4)){
+         giraIzquierdaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==1)){
+         giraDerechaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==3)&&(estado==2)){
+         avanzaAdelante();
+       }
+       else if((estadoCruce[numCruces-1]==2)&&(estado==4)){
+         giraDerechaAvanza();
+
+       }
+       else if((estadoCruce[numCruces-1]==2)&&(estado==1)){
+             giraIzquierdaAvanza();
+       }
+       else if((estadoCruce[numCruces-1]==2)&&(estado==3)){
+         avanzaAdelante();
+       }
+  }    
+  contadorCrucesDisponibles=0;  
+numCruces=numCruces-1;
+}    
+void giraIzquierda(){
+   drive_ramp(0, 0);
+ pause(100); 
+     drive_speed(-52,70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+ drive_speed(0,0);
+ if(estado==1){
+      estado=3;
+    } 
+    else if(estado==2){
+      estado=1;
+    } 
+    else if(estado==3){
+      estado=4;
+    }
+    else if(estado==4){
+      estado=2;
+    }  
+}  
+void giraDerecha(){
+   drive_ramp(0, 0);
+ pause(100); 
+     drive_speed(52,-70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+  drive_speed(0,0);
+if(estado==1){
+      estado=2;
+    } 
+    else if(estado==2){
+      estado=4;
+    } 
+    else if(estado==3){
+      estado=1;
+    }
+    else if(estado==4){
+      estado=3;
+    }   
+} 
+void avanzaAdelante(){
+    drive_ramp(64, 64);                       // Forward 2 RPS
+    if(regreso==0){
+    recorridoInverso[contadorRecorrido]=4;
+}  
+      
+  // While disatance greater than or equal
+  // to 20 cm, wait 5 ms & recheck.
+  while(ping_cm(8) >= distanciaMinima){ 
+  pause(5);           // Wait until object in range
+  tim=tim+5;
+  if(tim==temporizador){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+drive_ramp(0,0);
+}  
+void giraDerechaAvanza(){
+  drive_ramp(0,0); 
+    
+ pause(100); 
+     drive_speed(52,-70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+  drive_speed(0,0);
+      drive_ramp(64, 64);                       // Forward 2 RPS
+       if(regreso==0){
+    recorridoInverso[contadorRecorrido]=3;
+  }    
+      if(estado==1){
+      estado=2;
+    } 
+    else if(estado==2){
+      estado=4;
+    } 
+    else if(estado==3){
+      estado=1;
+    }
+    else if(estado==4){
+      estado=3;
+    }   
+    
+  // While disatance greater than or equal
+  // to 20 cm, wait 5 ms & recheck.
+  while(ping_cm(8) >= distanciaMinima){ 
+  pause(5);           // Wait until object in range
+  tim=tim+5;
+  if(tim==temporizador){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+drive_ramp(0,0);
+} 
+void giraIzquierdaAvanza(){
+    drive_ramp(0, 0);
+ pause(100); 
+     drive_speed(-52,70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+  drive_speed(0,0);
+      drive_ramp(64, 64);                       // Forward 2 RPS
+       if(regreso==0){
+    recorridoInverso[contadorRecorrido]=2;
+    }      
+    if(estado==1){
+      estado=3;
+    } 
+    else if(estado==2){
+      estado=1;
+    } 
+    else if(estado==3){
+      estado=4;
+    }
+    else if(estado==4){
+      estado=2;
+    }  
+  // While disatance greater than or equal
+  // to 20 cm, wait 5 ms & recheck.
+  while(ping_cm(8) >= distanciaMinima){ 
+  pause(5);           // Wait until object in range
+  tim=tim+5;
+  if(tim==temporizador){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+drive_ramp(0,0);
+}  
+void giraAtras(){
+  drive_ramp(0,0);
+    
+ pause(100); 
+     drive_speed(-52,70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+drive_speed(0,0);
+pause(100); 
+     drive_speed(-52,70);
+    while(1){ 
+  pause(1);           // Wait until object in range
+  tim=tim+1;
+  if(tim==425){
+    tim=0;
+    break;
+  }    
+}
+tim=0;
+drive_speed(0,0);
+  if(estado==1){
+      estado=4;
+    } 
+    else if(estado==2){
+      estado=3;
+    } 
+    else if(estado==3){
+      estado=2;
+    }
+    else if(estado==4){
+      estado=1;
+    }
+}   
+void validarIzquierda(){
+  freqout(11, 1, 38000);                      // Left IR LED light
+    irLeft = input(10);                         // Check left IR detector
+}  
+void validarAdelante(){
+  if(ping_cm(8)<=distanciaMinima){
+    frente=0;
+}
+  else{
+    frente=1;
+  }
+}  
+void validarDerecha(){
+  freqout(1, 1, 38000);                       // Repeat for right detector
+    irRight = input(2);
+}  
+
